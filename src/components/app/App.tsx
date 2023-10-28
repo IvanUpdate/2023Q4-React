d7170859c8548233c39a25a90e48d6b79603ba0b
IvanUpdate <mironovvan99@gmail.com>
2023-10-28 14:44:47 +0300

    add structure of the project

---
+++ src/components/app/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from  './App.module.css';
+
+class App extends React.Component {
+  render() {
+    return  <div className={styles.main}>Radio</div>
+  }
+}
+
+export default App;
