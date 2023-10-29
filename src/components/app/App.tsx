2163bb7ad01c066c3fabed4fc8ea3eceb5bf5aea
IvanUpdate <mironovvan99@gmail.com>
2023-10-29 20:09:06 +0300

    fix search issues

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -10,6 +10,7 @@ type AppProps = null;
 type AppState = {
   request: string;
   results: Array<Character> | null;
+  loading: boolean;
 };
 
 class App extends React.Component<AppProps, AppState> {
@@ -18,11 +19,16 @@ class App extends React.Component<AppProps, AppState> {
     this.state = {
       request: localStorage.getItem('request') || '',
       results: null,
+      loading: true,
     };
   }
+  
+  componentDidMount() {
+    this.handleSearch(this.state.request);
+  }
 
-  handleSearch = async () => {
-    const { request } = this.state;
+  handleSearch = async (request:string) => {
+    this.setState({ loading: true });
     const apiUrl = `https://rickandmortyapi.com/api/character/`;
     if (request.trim() === '') {
       try {
@@ -31,59 +37,62 @@ class App extends React.Component<AppProps, AppState> {
           const data = await response.json();
           this.setState({ results: data.results });
         } else {
+          this.setState({ results: null });
           console.error('API request failed');
         }
       } catch (error) {
         console.error('An error occurred:', error);
       }
+      this.setState({ loading: false });
+      return;
     }
 
     try {
+      localStorage.setItem('request', request.trim());
       const response = await fetch(`${apiUrl}?name=${request.trim()}`);
       if (response.ok) {
         const data = await response.json();
         this.setState({ results: data.results });
       } else {
+        this.setState({ results: null });
         console.error('API request failed');
       }
     } catch (error) {
       console.error('An error occurred:', error);
     }
+    this.setState({ loading: false });
   };
 
   render() {
     return (
       <div className={styles.main}>
         <div className={styles.search}>
-          <Search request={this.state.request} />
+          <Search request={this.state.request} handleSearch = {this.handleSearch}/>
         </div>
         <div className={styles.results}>
-          {this.state.results ? (
-            this.state.results.map((person) => {
-              return (
-                <CharacterItem
-                key={person.id}
-                name={person.name}
-                status={person.status}
-                type={person.type}
-                location={person.location.name}
-                image={person.image}
-                species={person.species}
-              />)
-            })
-          ) : (
+          {this.state.loading ? (
             <div className={styles.loader}>
               <img src={loader} />
             </div>
-          )}
-          <CharacterItem
-            name="Rick"
-            status="alive"
-            type=""
-            location="Earth"
-            image="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-            species="human"
-          />
+          ) : (this.state.results ? (
+            this.state.results.map((person) => {
+              return (
+                <CharacterItem
+                  key={person.id}
+                  name={person.name}
+                  status={person.status}
+                  type={person.type}
+                  location={person.location.name}
+                  image={person.image}
+                  species={person.species}
+                />
+              );
+            })
+          ) : (
+            <div className={styles.notFound}>
+              <p>Sorry, Not Found Results On Your Request</p>
+            </div>
+          ))}
         </div>
       </div>
     );
--- src/components/app/search/search.tsx
+++ src/components/app/search/search.tsx
@@ -3,32 +3,51 @@ import styles from './search.module.css';
 
 type SearchProps = {
   request: string;
+  handleSearch: (request: string) => void;
 };
 
-export class Search extends Component<SearchProps> {
+type SearchState = {
+  input: string;
+};
+
+export class Search extends Component<SearchProps, SearchState> {
+  constructor(props: SearchProps) {
+    super(props);
+    this.state = {
+      input: this.props.request,
+    };
+  }
+
+  handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ input: e.target.value });
+  };
+
   render() {
     const request = this.props.request;
+    const handleSearch = this.props.handleSearch;
 
     return (
       <div className={styles.search}>
         <div className={styles.wrapperInput}>
-        <input
-          type="text"
-          className={styles.searchInput}
-          placeholder={request}
-        />
+          <input
+            value={this.state.input}
+            onChange={this.handleInputChange}
+            type="text"
+            className={styles.searchInput}
+            placeholder={this.state.input}
+          />
         </div>
         <div className={styles.wrapperButton}>
-        <button>
-          <a href="#">
-            <div className={`${styles.scale} ${styles.rotate1}`}>s</div>
-            <div className={`${styles.scale} ${styles.rotate2}`}>e</div>
-            <div className={`${styles.scale} ${styles.rotate3}`}>a</div>
-            <div className={`${styles.scale} ${styles.rotate4}`}>r</div>
-            <div className={`${styles.scale} ${styles.rotate5}`}>c</div>
-            <div className={`${styles.scale} ${styles.rotate6}`}>h</div>
-          </a>
-        </button>
+          <button onClick={() => handleSearch(this.state.input)}>
+            <div className={styles.apply}>
+              <div className={`${styles.scale} ${styles.rotate1}`}>s</div>
+              <div className={`${styles.scale} ${styles.rotate2}`}>e</div>
+              <div className={`${styles.scale} ${styles.rotate3}`}>a</div>
+              <div className={`${styles.scale} ${styles.rotate4}`}>r</div>
+              <div className={`${styles.scale} ${styles.rotate5}`}>c</div>
+              <div className={`${styles.scale} ${styles.rotate6}`}>h</div>
+            </div>
+          </button>
         </div>
       </div>
     );
